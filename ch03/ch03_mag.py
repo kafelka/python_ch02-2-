178aad57ac35a656daa450fb8d22464de3dfaf93
Mag Kafel <na.hasiok@gmail.com>
2018-12-03 12:26:33 +0000

    Further exercises before midday

--- ch03/ch03_mag.py
+++ ch03/ch03_mag.py
@@ -68,14 +68,99 @@ Created on Thu Nov 29 13:54:59 2018
 ##add_two_numbers()
 #add_two_numbers(a1,b1)
 
-def tempConverter(celsius):
-    
-    fahrenheit = celsius * 9.0 /5.0 + 32
-    kelvin = celsius + 273.15
-    
-    print("That's {} degrees in fahrenheit and {} degrees in kelvin.".format(fahrenheit, kelvin))
-    
-celsius = int(input("What's the temperature in your city today?"))
-    
+#def tempConverter(celsius):
+#    
+#    fahrenheit = celsius * 9.0 /5.0 + 32
+#    kelvin = celsius + 273.15
+#    
+#    print("That's {} degrees in fahrenheit and {} degrees in kelvin.".format(fahrenheit, kelvin))
+#    
+#celsius = int(input("What's the temperature in your city today?"))
+#    
+#tempConverter(celsius)
 
-tempConverter(celsius)
+#####the below does not work!!!!!!!!!!
+#celsius = int(input("What's the temperature in your city today?"))
+#
+#def tempConverter2(celsius):
+#    
+#    fahrenheit = celsius * 9.0 /5.0 + 32
+#    kelvin = celsius + 273.15
+#    
+#    return "That's {} degrees in fahrenheit and {} degrees in kelvin.".format(fahrenheit, kelvin)
+#    
+#tempConverter2(celsius)
+
+
+
+#def tempConv(celsius):
+#    fahrenheit = celsius * 9.0 /5.0 + 32
+#    kelvin = celsius + 273.15
+#    return (fahrenheit, kelvin)
+#
+#celsius = 10
+#
+#fahrenheit, kelvin = tempConv(celsius)
+#print(fahrenheit, kelvin)
+
+
+#def convTemp():
+#    celsius = int(input("What's the temperature in your city today?"))
+#    fahrenheit = celsius * 9.0 /5.0 + 32
+#    kelvin = celsius + 273.15
+#    print("Converting temp in c to fahrenheit and kelvin...")
+#    print("That's {} degrees in fahrenheit and {} degrees in kelvin.".format(fahrenheit, kelvin))
+#    return (fahrenheit, kelvin)
+#
+##convTemp()
+#kelvin, fahrenheit = convTemp()
+
+
+#def convert_temp_kelvin(celsius):
+#    kelvin = celsius + 273.15
+#    
+#    return kelvin
+#
+#def convert_temp_fahr(celsius):
+#    fahr = (celsius + 9.0) / 5.0 = 32
+#    
+#    return fahr
+#
+#def convert_temp_kelvin_to_fahr(celsius):
+#    kelvin1 = convert_temp_kelvin(celsius)
+#    fahr = ((kelvin1 * 9) /5) - 459.67
+#            
+#    print("Converting temp in kelvin to fahr.")
+#    print("Temp in kelvin:", kelvin1)
+#    print("Temp in fahr:", fahr)
+#    
+#    mainResult = kelvin1
+#    return mainResult
+#
+#mainResult = convert_temp_kelvin_to_fahr(celsius2)
+
+
+#def convertDistance(miles):
+#    kilometers = (miles * 1.609)
+#    print("Converting distance in miles to kilometers.")
+#    print("Distance in miles:", miles)
+#    print("Distance in kilometers:", kilometers)
+#    
+#    return ("miles:" + str(miles), "kilometeres" + str(kilometers))
+#
+#convertDistance(10)
+
+
+def convertDistance():
+    miles = int(input("How many miles did you run last weekend?"))
+    kilometers = (miles * 1.61)
+    print("The distance you run is {} in miles and {} in kilometers.".format(miles, kilometers))
+    
+    return (miles, kilometers)
+
+convertDistance()
+
+London_Bristol = convertDistance(118)
+London_Cardiff = convertDistance(150.6)
+London_Brighton = convertDistance(67)
+London_Birmingham = convertDistance(126)
