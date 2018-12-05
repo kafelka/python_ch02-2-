485c70429df71a564d457b80d171493263aebc27
Mag Kafel <na.hasiok@gmail.com>
2018-12-05 15:18:46 +0000

    Human-climber classes exercise

--- ch05/ch05_mag_py.py
+++ ch05/ch05_mag_py.py
@@ -54,8 +54,8 @@ maggie.withdraw(200.0)
 #withdrawAmount = float(input("How much did you withdraw today?"))
 #print(maggie.withdraw(withdrawAmount))
 
-name1 = "Clare B"
-balance1 = 3000.0
+#name1 = "Clare B"
+#balance1 = 3000.0
 
 #print("Type:", type(jason))
 
@@ -66,19 +66,19 @@ balance1 = 3000.0
 #print(list(range(1,10,3)))
 
 
-class Animal():
-    def eat(self):
-        print("Yum yum yum!")
-        
-class Dog(Animal):
-    def bark(self):
-        print("Woof, woof!")
-        
-class Cat(Animal):
-    def meow(self):
-        print("Meooow!")
-    def paws(self):
-        print("I've got 4 paws.")
+#class Animal():
+#    def eat(self):
+#        print("Yum yum yum!")
+#        
+#class Dog(Animal):
+#    def bark(self):
+#        print("Woof, woof!")
+#        
+#class Cat(Animal):
+#    def meow(self):
+#        print("Meooow!")
+#    def paws(self):
+#        print("I've got 4 paws.")
         
 #Snoopy = Dog()
 #Snoopy.bark()
@@ -89,31 +89,120 @@ class Cat(Animal):
 #Cosmo.paws()
 #Cosmo.eat()
 
-class Robot():
-    def jump(self):
-        print("Jumping to warm up.")
-        
-class Climber(Robot):
-    def climbUp(self):
-        print("I'm climbing up the wall.")
-        
-class Overhang(Climber):
-    def overhangUp(self):
-        print("I'm climbing up an overhanging wall.")
-        
-class Climber2(Robot):
-    def climbDown(self):
-        print("I'm climbing down.")
-        
-class Helper(Robot):
-    def help(self):
-        print("I am carrying heavy stuff.")
-        
-Morty = Helper()
-Morty.help()
+#class Robot():
+#    def jump(self):
+#        print("Jumping to warm up.")
+#        
+#class Climber(Robot):
+#    def climbUp(self):
+#        print("I'm climbing up the wall.")
+#        
+#class Overhang(Climber):
+#    def overhangUp(self):
+#        print("I'm climbing up an overhanging wall.")
+#        
+#class Climber2(Robot):
+#    def climbDown(self):
+#        print("I'm climbing down.")
+#        
+#class Helper(Robot):
+#    def help(self):
+#        print("I am carrying heavy stuff.")
+#        
+#Morty = Helper()
+#Morty.help()
+#
+#Max = Overhang()
+#Max.overhangUp()
 
-Max = Overhang()
-Max.overhangUp()
+#Chen's example
+#class Animal():
+#    def __init__(self, name, age=0):
+#        self.name = name
+#        self.age = age
+#        
+#    def eat(self):
+#         print('yum')
+#         
+#class Dog(Animal):
+#    def __init__(self, name, age=0,barkNumber=0):
+#        self.barkNumber = barkNumber
+#        
+#    def bark(self):
+#        print('Woof! '*self.barkNumber)
+#        
+#        
+#class DogAgent(Dog):
+#    def detect(self):
+#        if self.barkNumber>=3:
+#            print('strenger coming!!!')
+#        
+#class Cat(Animal):
+#    def meow(self):
+#        print('Meow')
+#name = input('what is your pet\'s name:')        
+#age = int(input('what is your pet\'s age: '))
+#bark = int(input('how many times you heard it barked: '))
+#
+#dog007 = DogAgent(name, age, bark) #always inheritant ancester's
+#dog007.bark()
+#dog007.eat()
+#dog007.detect()
+
+class Human():
+    def __init__(self, name, age=0, stamina=0):
+        self.name = name
+        self.age = age
+        self.stamina = stamina
+        
+        def greeting(self):
+            print(f"Hello {self.name}!")
+            
+class Climber(Human):            
+    def __init__(self, name, age=0, stamina=0, experience=0, routes=0):
+        Human.__init__(self, name, age=0, stamina=0, experiece=0, routes=0)
+        self.routes = routes
+        self.experience = experience
+        
+        def stamina(self):
+#            if self.experience < 1:
+#                self.stamina = 1
+#                print("I bet you have a lot of fun. Keep doing it!")
+            if self.experience > 1 and self.experience < 2:
+                self.stamina = 2
+                print("You have less than " + self.stamina + " years of climbing experience. I bet you like challenges!")
+            elif self.experience > 2 and self.experience < 4:
+                self.stamina = 3
+                print("You are a quite strong climber, have you tried v6s yet? ;-)")
+            else:
+                self.stamina = 4
+                print(f"You have over {self.stamina} years of climbing experience. All you need is some chalk and coffee.")
+
+#class ModerateCoffeeDrinker(Climber):
+#    def __init__(self, name, age=0, stamina=0, experience=0, routes=0, espresso=1)
+#        self.espresso = espresso
+#        
+#        def completedRoutes:
+#            self.espresso * self.stamina
+#        
+#        
+#    
+#class CoffeeAddict(Climber):
+#    def __init__(self, name, age=0, stamina=0, experience=0, routes=0, espresso=3)
+#    self.espresso = espresso
+#    
+    
+class NonClimber(Human):
+    def __init__(self, name, age=0, stamina=0, routes=0, motivation=0):
+        self.routes = routes
+        self.motivation = motivation
+        
+        
+name = input("What's your name? ")
+        
+
+
+
 
 
         
