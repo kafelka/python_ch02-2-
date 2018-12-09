b87b43c7a9f0a0082ea00df4ce756ef54d1e3113
Mag Kafel <na.hasiok@gmail.com>
2018-12-09 21:45:02 +0000

    ex.3 completed

--- ch06/codingbat_logic1.py
+++ ch06/codingbat_logic1.py
@@ -68,8 +68,17 @@ print("\n")
 #squirrel_play(95, True) → True
 
 def squirrel_play(temp, is_summer):
-    return
-   
+    if temp >= 60 and temp <= 90:
+        return True
+    elif is_summer == True and (temp >= 60 and temp <= 100):
+        return True
+    else:
+        return False
+ 
+print(squirrel_play(70, False))
+print(squirrel_play(95, False))
+print(squirrel_play(95, True))
+print(squirrel_play(55, True))
     
 ############################
 print("\n")
