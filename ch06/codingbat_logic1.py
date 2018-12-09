91d18903c389d77605b051c0d9f5a05d23c9f518
Mag Kafel <na.hasiok@gmail.com>
2018-12-09 21:55:58 +0000

    ex.4

--- ch06/codingbat_logic1.py
+++ ch06/codingbat_logic1.py
@@ -82,3 +82,39 @@ print(squirrel_play(55, True))
     
 ############################
 print("\n")
+
+#You are driving a little too fast, and a police officer stops you. 
+#Write code to compute the result, encoded as an int value: 0=no ticket, 
+#1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed 
+#is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the 
+#result is 2. Unless it is your birthday -- on that day, your speed can be 5 higher 
+#in all cases.
+#
+#
+#caught_speeding(60, False) → 0
+#caught_speeding(65, False) → 1
+#caught_speeding(65, True) → 0
+
+def caught_speeding(speed, is_birthday):
+    if speed <= 60 or (is_birthday == True and speed <= 65):
+        return 0
+    elif (speed >= 61 and speed <= 80) or (is_birthday == True and speed <= 85):
+        return 1
+    elif speed >= 82 or is_birthday == True and speed <= 87:
+        return 2
+
+    
+print(caught_speeding(50, False))
+print(caught_speeding(70, False))
+print(caught_speeding(70, True))
+############################
+print("\n")
+
+
+############################
+print("\n")
+
+
+
+############################
+print("\n")
