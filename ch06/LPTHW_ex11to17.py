4b26f973e5e5004bf8e37253591266f6ae737de6
Mag Kafel <na.hasiok@gmail.com>
2018-12-10 14:10:44 +0000

    Initiated LPTHW exercises 11 to 17

---
+++ ch06/LPTHW_ex11to17.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 10 14:05:18 2018
+
+@author: nahas
+"""
+
+print("How old are you?", end=" ")
+age = input()
+print("How tall are you?", end = " ")
+height = input()
+print("How much do you weigh?", end= " ")
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
