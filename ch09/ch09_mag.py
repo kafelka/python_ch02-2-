e62dc10845534162bfb169e94fef0a33ee1e4369
Mag Kafel <na.hasiok@gmail.com>
2018-12-12 16:24:58 +0000

    Chapter 9 tasks

--- ch09/ch09_mag.py
+++ ch09/ch09_mag.py
@@ -4,51 +4,121 @@ Created on Wed Dec 12 12:16:04 2018
 
 @author: nahas
 """
-my_fav_fruit = ["apple", "raspberry", "grapefruit"]
-print(my_fav_fruit)
-print(my_fav_fruit[0])
+#my_fav_fruit = ["apple", "raspberry", "grapefruit"]
+#print(my_fav_fruit)
+#print(my_fav_fruit[0])
+#
+#x = ["this", 55, "that", my_fav_fruit]
+#print(x)
+#print(x[0])
+#print(x[1])
+#print(x[3])
+#print(x[3][0])
+##print(x[4])
+#
+#print("*******************")
+#x.remove(my_fav_fruit)
+#print(x)
+#x[1] = "and"   #replacing/overwriting/updating item with index 1
+#print(x)
+#
+#x.append("again") #appending/extending
+#print(x)
+#
+#a = [0, 2, 3, 2] #removes the first matching value (not returning)
+#a.remove(2)
+#print(a)
+#
+#b = [3, 2, 2, 1] # removes the item at a specific index (not returning)
+#del b[1]
+#print(b)
+#
+#c = [4, 3, 5]  #removes the item at a specific index and returns it
+#c.pop(1)
+#print(c)
+#
+#d = [1, 2, 3, 4, 5, 6]
+#del d[2:]
+#print(d)
+#
+#print("*******************")
+#e = ["the", "cat", "sat"]
+#f = ["on", "the", "mat"]
+#g = e + f
+#print(g)
+#
+#h = 2 * e
+#print(h)
+#print("*******************")
+#print(set(e+f))
+#
+#k = ["this", "and", "that", "once", "again"]
+#print(k)
+#print(k[1:4])
+#print(k)
+#
+#z = ["the", "cat", "sat", "on", "the", "mat"]
+#print(z[3:10])
+#print(z[8:10])
+#print("*******************")
+#
+#m = [7,11,3,9,2]
+#n = sorted(m)
+#print(n)
+#print(m)
+#print("*******************")
+#
+#r = [7,11,3,9,2]
+#print(r)
+#print(sorted(r))
+#
+#sorted(r,reverse = True) #reversing changes
+#print(r)
 
-x = ["this", 55, "that", my_fav_fruit]
-print(x)
-print(x[0])
-print(x[1])
-print(x[3])
-print(x[3][0])
-#print(x[4])
+print("tuples*******************")
 
-print("*******************")
-x.remove(my_fav_fruit)
-print(x)
-x[1] = "and"   #replacing/overwriting/updating item with index 1
-print(x)
-
-x.append("again") #appending/extending
-print(x)
-
-a = [0, 2, 3, 2] #removes the first matching value (not returning)
-a.remove(2)
+a = [0,1,2,3,4,5,6,7,8,9]
+print(a)
+del a[-1]
+print(a)
+del a[-3]
+print(a)
+del a[-3:]
 print(a)
 
-b = [3, 2, 2, 1] # removes the item at a specific index (not returning)
-del b[1]
+b = (0,1,2,3,4,5,6,7,8,9)
+#del b[-1]  tuple object does not support item deletion
 print(b)
 
-c = [4, 3, 5]  #removes the item at a specific index and returns it
-c.pop(1)
-print(c)
+a = [0,1,2,3,4,5,6,7,8,9]
+a[0] = 50
+print(a)
 
-d = [1, 2, 3, 4, 5, 6]
-del d[2:]
-print(d)
+b = (0,1,2,3,4,5,6,7,8,9)
+#b[0] = 50 tuple object does not support item assignment
+print(b)
+
+a.append("z")
+print(a)
+
+#b.append("z") #tuple object has no attribute 'append'
+print(b)
 
 print("*******************")
-e = ["the", "cat", "sat"]
-f = ["on", "the", "mat"]
-g = e + f
-print(g)
+x = ["the", "cat", "on"]
+y = [7,11,3,9,2]
+z = [2,3,7,8,11]
+x2 = [("a",3,z), ("c",1,y), ("b",5,x)]
+sorted(x2)
+print(x2)
 
-h = 2 * e
-print(h)
+z[0]=22
+z[1]=5
+
+x2 = [("a",3,z), ("c",1,y), ("b",5,x)]
+
+sorted(x2, key=lambda s:s[2])
+print(x2)
 print("*******************")
-print(set(e+f))
-
+sorted(x2, key=lambda s:s[2][1])
+print(x2)
