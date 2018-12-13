d1074fbc88f07b3208f01cb7507d79bfdbbf58c5
Mag Kafel <na.hasiok@gmail.com>
2018-12-13 09:57:21 +0000

    Initialised LPTHW exercises 34-38, updated ch09 with in class examples

---
+++ ch09/LPTHW_ex34-38.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 13 09:30:47 2018
+
+@author: nahas
+"""
+#ex34
+animals = ["bear", "python v3.", "peacock", "kangaroo", "whale", "platypus"]
+
+#The animal at 1.
+#The third (3rd) animal.
+#The first animal.
+#The animal at 3.
+#The fifth animal.
+#The animal at 2.
+#The sisth animal.
+#The animal at 4.
+
--- ch09/ch09_mag.py
+++ ch09/ch09_mag.py
@@ -4,76 +4,89 @@ Created on Wed Dec 12 12:16:04 2018
 
 @author: nahas
 """
-#my_fav_fruit = ["apple", "raspberry", "grapefruit"]
-#print(my_fav_fruit)
-#print(my_fav_fruit[0])
-#
-#x = ["this", 55, "that", my_fav_fruit]
-#print(x)
+my_fav_fruit = ["apple", "raspberry", "grapefruit"]
+print(my_fav_fruit)
+print(my_fav_fruit[0])
+
+x = ["this", 55, "that", my_fav_fruit]
+print(x)
 #print(x[0])
 #print(x[1])
 #print(x[3])
+#print("****** result for x[-1][-2]:")
+#print(x[-1][-2])
+#print("result of [x[-2][-3]]:")
+#print(x[-2][-3])
 #print(x[3][0])
+#
+#print("result of x[-1][-2][-2][0]")
+#print(x[-1][-2][-2][0])
 ##print(x[4])
-#
-#print("*******************")
-#x.remove(my_fav_fruit)
-#print(x)
-#x[1] = "and"   #replacing/overwriting/updating item with index 1
-#print(x)
-#
-#x.append("again") #appending/extending
-#print(x)
-#
-#a = [0, 2, 3, 2] #removes the first matching value (not returning)
-#a.remove(2)
-#print(a)
-#
-#b = [3, 2, 2, 1] # removes the item at a specific index (not returning)
-#del b[1]
-#print(b)
-#
-#c = [4, 3, 5]  #removes the item at a specific index and returns it
-#c.pop(1)
-#print(c)
-#
-#d = [1, 2, 3, 4, 5, 6]
-#del d[2:]
-#print(d)
-#
-#print("*******************")
-#e = ["the", "cat", "sat"]
-#f = ["on", "the", "mat"]
-#g = e + f
-#print(g)
-#
-#h = 2 * e
-#print(h)
-#print("*******************")
-#print(set(e+f))
-#
-#k = ["this", "and", "that", "once", "again"]
-#print(k)
-#print(k[1:4])
-#print(k)
-#
-#z = ["the", "cat", "sat", "on", "the", "mat"]
-#print(z[3:10])
-#print(z[8:10])
-#print("*******************")
-#
-#m = [7,11,3,9,2]
-#n = sorted(m)
-#print(n)
-#print(m)
-#print("*******************")
-#
-#r = [7,11,3,9,2]
-#print(r)
-#print(sorted(r))
-#
-#sorted(r,reverse = True) #reversing changes
-#print(r)
+
+print("*******************")
+print("print x before removal: ", x)
+x.remove(my_fav_fruit)
+print("print x after removal:", x)
+x[1] = "and"   #replacing/overwriting/updating item with index 1
+print(x)
+
+x.append("again") #appending/extending the list ( as last element)
+print(x)
+
+y = x.append("hello")
+print(y)
+print(type(y))
+print(x)
+
+a = [0, 2, 3, 2] #removes the first matching value (not returning)
+a.remove(2)
+print(a)
+
+b = [3, 2, 2, 1] # removes the item at a specific index (not returning)
+del b[1]
+print(b)
+
+c = [4, 3, 5]  #removes the item at a specific index and returns it
+c.pop(1)
+print(c)
+
+d = [1, 2, 3, 4, 5, 6]
+del d[2:]
+print(d)
+
+print("*******************")
+e = ["the", "cat", "sat"]
+f = ["on", "the", "mat"]
+g = e + f
+print(g)
+
+h = 2 * e
+print(h)
+print("*******************")
+print(set(e+f))
+
+k = ["this", "and", "that", "once", "again"]
+print(k)
+print(k[1:4])
+print(k)
+
+z = ["the", "cat", "sat", "on", "the", "mat"]
+print(z[3:10])
+print(z[8:10])
+print("*******************")
+
+m = [7,11,3,9,2]
+n = sorted(m)
+print(n)
+print(m)
+print("*******************")
+
+r = [7,11,3,9,2]
+print(r)
+print(sorted(r))
+
+sorted(r,reverse = True) #reversing changes
+print(r)
 
 print("tuples*******************")
 
@@ -117,8 +130,8 @@ z[1]=5
 
 x2 = [("a",3,z), ("c",1,y), ("b",5,x)]
 
-sorted(x2, key=lambda s:s[2])
-print(x2)
-print("*******************")
-sorted(x2, key=lambda s:s[2][1])
-print(x2)
+#sorted(x2, key=lambda s:s[2])
+#print(x2)
+#print("*******************")
+#sorted(x2, key=lambda s:s[2][1])
+#print(x2)
