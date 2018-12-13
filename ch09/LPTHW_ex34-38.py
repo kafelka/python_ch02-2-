a8959e7298849a696cd47ea2a782a33690151164
Mag Kafel <na.hasiok@gmail.com>
2018-12-13 12:28:47 +0000

    LPTHW ex.34

--- ch09/LPTHW_ex34-38.py
+++ ch09/LPTHW_ex34-38.py
@@ -5,14 +5,23 @@ Created on Thu Dec 13 09:30:47 2018
 @author: nahas
 """
 #ex34
-animals = ["bear", "python v3.", "peacock", "kangaroo", "whale", "platypus"]
+animal = ["bear", "python v3.", "peacock", "kangaroo", "whale", "platypus"]
 
 #The animal at 1.
+print(animal[1])
 #The third (3rd) animal.
+print(animal[2])
 #The first animal.
+print(animal[0])
 #The animal at 3.
+print(animal[3])
 #The fifth animal.
+print(animal[4])
 #The animal at 2.
-#The sisth animal.
+print(animal[2])
+#The sixth animal.
+print(animal[5])
 #The animal at 4.
+print(animal[4])
 
+#ex35
