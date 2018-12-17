1accb4b527daa4efdab3a24ec2d757980dd591ad
Mag Kafel <na.hasiok@gmail.com>
2018-12-17 18:01:07 +0000

    Improved queen's age diff function

--- ch10/phonebook.py
+++ ch10/phonebook.py
@@ -6,22 +6,20 @@ Created on Mon Dec 17 15:09:58 2018
 """
 
 phoneBook = {"Joan": ("753", 13, "NW3 5GH", "London", 34),
-             "Edi": ("225", 7, "AB10 1EZ", "Aberdeen", 28),
+             "Eddie": ("225", 7, "AB10 1EZ", "Aberdeen", 28),
              "Natalie": ("477", 5, "L1 0RD", "Liverpool", 26),
              "Veronica": ("850", 21, "M1 1FR", "Manchester", 30)
              }
 
 
 def addToPhoneBook():
-    name = input('What is your first name? ')
+    name = input('What is your first name? ').title()
     last3DigitNo = input('What are the last three digit of your phone no.? ')
     luckNo = int(input('What is your lucky no.? '))
-    postCode = input('What is your post code? ')
-    townCity = input('Which city do you come from? ')
-    age = input("How old are you? ")
+    postCode = input('What is your post code? ').upper()
+    townCity = input('Which city do you come from? ').title()
+    age = int(input("How old are you? "))
     return {name: (last3DigitNo, luckNo, postCode, townCity, age)}
-
-
 
 #newEntry = addToPhoneBook()
 newEntry = {}
@@ -44,20 +42,45 @@ def sortByLuckyNo():
 def sortByAge():
     sortBy = sorted(phoneBook.items(), key=lambda kv:kv[1][4])
     print("\nPhone book sorted by age:\n", sortBy)
-       
-sortByName()
-sortByCity()
-sortByLuckyNo()
-sortByAge()
-
-def queensAgeDifference():
-    queensAge = (2018 - 1926)
-    for name in phoneBook.keys():
-        userAge = phoneBook[name][4]
-        difference = queensAge - userAge
-        print(f"\nThe difference between {name}'s age and queen's age is: \n", difference)
     
-queensAgeDifference()
+def sortByChoice():
+    choice = input("""
+What would you like to print the phone book by?
+Select:
+   * 1 for sorting by name
+   * 2 for sorting by city
+   * 3 for sorting by lucky number
+   * 4 for sorting by age\n """)
+        
+    if choice == "1":
+        sortByName()
+    elif choice == "2":
+        sortByCity()
+    elif choice == "3":
+        sortByLuckyNo()
+    else:
+        sortByAge()
+        
+sortByChoice()       
+
+#def queensAgeDifference():
+#    queensAge = (2018 - 1926)
+#    for name in phoneBook.keys():
+#        userAge = phoneBook[name][4]
+#        difference = queensAge - userAge
+#        print(f"\nThe difference between {name}'s age and queen's age is: \n", difference)
+#    
+#queensAgeDifference()
+
+def queen():
+    queensAge = (2018 - 1926)
+    colleagues = ", ".join(list(sorted(phoneBook)))
+    selectName = input(f"Type name of your colleague in order to find the difference in age between then and the Queen. List of colleagues: {colleagues}: \n") 
+    userAge = phoneBook[selectName][4]
+    result = queensAge - userAge
+    print(f"The age difference between the Queen and {selectName} is: {result}.")
+
+queen()
 
 def printPhoneBookToFile(dictionary):
 # with guarantees closing of the resource (the variable after "as")
