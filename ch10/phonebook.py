62bdf3b0e789c430a28562987c39946604eb40bf
Mag Kafel <na.hasiok@gmail.com>
2018-12-17 16:49:52 +0000

    Phone book updated with queen's age diff function and saving to file

--- ch10/phonebook.py
+++ ch10/phonebook.py
@@ -31,16 +31,39 @@ print(phoneBook)
 
 def sortByName():
     sortBy = sorted(phoneBook.items(), key=lambda kv:kv[0])
-    print("\nPhone book sorted by \n", sortBy)
+    print("\nPhone book sorted by name:\n", sortBy)
     
 def sortByCity():
     sortBy = sorted(phoneBook.items(), key=lambda kv:kv[1][3])
-    print("\nPhone book sorted by \n", sortBy)
+    print("\nPhone book sorted by city:\n", sortBy)
 
 def sortByLuckyNo():
     sortBy = sorted(phoneBook.items(), key=lambda kv:kv[1][1])
-    print("\nPhone book sorted by \n", sortBy)
-    
+    print("\nPhone book sorted by lucky no:\n", sortBy)
+
+def sortByAge():
+    sortBy = sorted(phoneBook.items(), key=lambda kv:kv[1][4])
+    print("\nPhone book sorted by age:\n", sortBy)
+       
 sortByName()
 sortByCity()
 sortByLuckyNo()
+sortByAge()
+
+def queensAgeDifference():
+    queensAge = (2018 - 1926)
+    for name in phoneBook.keys():
+        userAge = phoneBook[name][4]
+        difference = queensAge - userAge
+        print(f"\nThe difference between {name}'s age and queen's age is: \n", difference)
+    
+queensAgeDifference()
+
+def printPhoneBookToFile(dictionary):
+# with guarantees closing of the resource (the variable after "as")
+# after the program continues outside the with block
+    with open('phoneBook.txt', 'w') as f:
+        f.write(str(dictionary))
+
+#printPhoneBookToFile(phoneBook)   
+    
