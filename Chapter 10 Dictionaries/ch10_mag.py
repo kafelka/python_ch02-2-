b992d79a81c4040da0fadb7458c2294fb0e6d495
Mag Kafel <na.hasiok@gmail.com>
2018-12-19 14:13:24 +0000

    Uncommenting ch10, adding exercise info

--- Chapter 10 Dictionaries/ch10_mag.py
+++ Chapter 10 Dictionaries/ch10_mag.py
@@ -4,86 +4,89 @@ Created on Fri Dec 14 09:56:06 2018
 
 @author: mag
 """
+#ex1 creating a dictionry, assigning and updating values
+salary = {}
+print(salary)
 
-#salary = {}
-#print(salary)
-#
-#salary['al'] = 20000
-#print(salary)
-#
-#salary["name"] = "mag"
-#print(salary)
-#
-#salary[2] = 20
-#print(salary)
-#
-#salary["al"] = 20500
-#print(salary)
-#
-#salary[7] = ("Joke", "Chen", "Bond")
-#print(salary)
-#
-#salary["bo"] = 50000
-#print(salary)
-#
-#salary["al"] += 2000
-#print(salary)
-#
-#tel = {"sarika":156, "martina":380, "mag": 241}
-#print(tel)
-#
-#print(tel["sarika"])
-#tel["sarika"] = 1156
-#tel["martina"] = 3380
-#tel["mag"] = 1041
-#print(tel)
-#tel["kirsty"] = 4290
-#print(tel)
-#
-#del tel["mag"]
-#print(tel)
-#
-#keysTel = tel.keys()
-#valuesTel = tel.values()
-#print(keysTel)
-#print(valuesTel)
-#print(type(valuesTel))
-#
-#convert keys and values to list data type
-#print(list(tel.keys()))
-#print(list(tel.values()))
-#
-#in order to avoid errors, we can run a test:
-#k = "mag"
-#
-#if k in tel:
-#    print(k, ":", tel[k])
-#else:
-#    print(k, ":", "not found!")
-#    
-#if "sarika" in tel:
-#    print("sarika", ":", tel["sarika"])
-#else:
-#    print("sarika", "not found")
-#    
-#    
-#counts = {"a": 3, "c":1, "b": 5}
-#labels = list(counts.keys())
-#labels.sort(key=lambda k:counts[k])
-#print(counts)
-#print(labels)
-#
-#counts["g"] = 8
-#counts["f"] = 10
-#labels = list(counts.keys())
-#labels.sort(key=lambda k:counts[k])
-#
-#print(counts)
-#print(labels)
-#labels = list(counts.keys())
-#labels.sort(key=lambda k:counts[k])
-#   
-#
+salary['al'] = 20000
+print(salary)
+
+salary["name"] = "mag"
+print(salary)
+
+salary[2] = 20
+print(salary)
+
+salary["al"] = 20500
+print(salary)
+
+salary[7] = ("Joke", "Chen", "Bond")
+print(salary)
+
+salary["bo"] = 50000
+print(salary)
+
+salary["al"] += 2000
+print(salary)
+
+#ex2, ex3 create your own dictionary, look up and delete values
+tel = {"sarika":156, "martina":380, "mag": 241}
+print(tel)
+
+print(tel["sarika"])
+tel["sarika"] = 1156
+tel["martina"] = 3380
+tel["mag"] = 1041
+print(tel)
+tel["kirsty"] = 4290
+print(tel)
+
+del tel["mag"]
+print(tel)
+
+#ex4 keys and values - retrieving them from dictionary
+
+keysTel = tel.keys()
+valuesTel = tel.values()
+print(keysTel)
+print(valuesTel)
+print(type(valuesTel))
+
+#ex5 convert keys and values to list data type
+print(list(tel.keys()))
+print(list(tel.values()))
+
+#ex6 in order to avoid errors, we can run a test:
+k = "mag"
+
+if k in tel:
+    print(k, ":", tel[k])
+else:
+    print(k, ":", "not found!")
+    
+if "sarika" in tel:
+    print("sarika", ":", tel["sarika"])
+else:
+    print("sarika", "not found")
+    
+    
+counts = {"a": 3, "c":1, "b": 5}
+labels = list(counts.keys())
+labels.sort(key=lambda k:counts[k])
+print(counts)
+print(labels)
+
+counts["g"] = 8
+counts["f"] = 10
+labels = list(counts.keys())
+labels.sort(key=lambda k:counts[k])
+
+print(counts)
+print(labels)
+labels = list(counts.keys())
+labels.sort(key=lambda k:counts[k])
+   
+
 abc = {} 
 abc = {
        1: ("greg","january",7 ),
@@ -94,7 +97,7 @@ abc = {
 print(list(abc.keys()))
 print(list(abc.values()))
 
-
+#ex7 & ex8 sorting dictionaries
 abc_keys = list(abc.keys())
 abc_keys.sort(key=lambda k:abc[k][2])
 print("second value keys:", abc_keys)
@@ -112,50 +115,50 @@ for i in abc.items():
     print (i)
 
 print(sorted(abc.items(), key=lambda kv:kv[0]))
-#counts = {"a": 3, "c":1, "b": 5}
-#sorted(counts.items(), key=lambda kv:kv[1])   
-#print(counts)
-#sorted(counts.items(), key=lambda kv counts[k])
+counts = {"a": 3, "c":1, "b": 5}
+sorted(counts.items(), key=lambda kv:kv[1])   
+print(counts)
 
 print(sorted(abc, key=lambda k: abc[k]))
 print(sorted(abc, key=lambda k: abc[k][2]))
 
-#reverse order
+#reverse order when sorting (descending order)
 abc_keys.sort(reverse=True, key = lambda k:abc[k][2])
 
 sorted(abc, key=lambda k:abc[k][2], reverse=True)
 sorted(abc.items(), key=lambda kv: kv[1][2], reverse=True)
 
-#price per gram in USD
+
 metals = {}
 metals = {
         "gold": (19320, 40.81, 10),
         "silver": (10490, 0.53, 100),
         "platinum": (21400, 1, 1)
         }
+#* second value = price per gram in USD
 
-#print(metals)
-#print("***********")
-#print("list keys", list(metals.keys()))
-#print("list values", list(metals.values()))
-#
-#metals_keys = list(metals.keys())
-#print(metals_keys)
-#metals_values = list(metals.values())
-#
-#print("***********")
-#
-#metals_keys.sort(key=lambda k:metals[k][2])
-#print("sort by second value keys:", metals_keys)
-#metals_keys.sort(key=lambda k:metals[k][1])
-#print("sort by first value keys:", metals_keys)
-#
-#print("***********")
-#print(sorted(metals, key=lambda k:metals[k][2], reverse=True))
-#print(metals_values)
+print(metals)
+print("***********")
+print("list keys", list(metals.keys()))
+print("list values", list(metals.values()))
+
+metals_keys = list(metals.keys())
+print(metals_keys)
+metals_values = list(metals.values())
+
+print("***********")
+
+metals_keys.sort(key=lambda k:metals[k][2])
+print("sort by second value keys:", metals_keys)
+metals_keys.sort(key=lambda k:metals[k][1])
+print("sort by first value keys:", metals_keys)
+
+print("***********")
+print(sorted(metals, key=lambda k:metals[k][2], reverse=True))
+print(metals_values)
 y = sorted(metals,key = lambda k:metals[k][2])
 print(y)
 x = sorted(metals.items(), key=lambda kv: kv[1][2], reverse=True)
 print(x)
 
-#print(metals_values)
+print(metals_values)
