4a01e09297de033fcb7e25c5b5eb352fbf639cd6
Mag Kafel <na.hasiok@gmail.com>
2018-12-19 11:51:53 +0000

    Uncommenting file

--- Chapter 2 Operations, strings and variables/ch02_mag.py
+++ Chapter 2 Operations, strings and variables/ch02_mag.py
@@ -2,82 +2,85 @@
 """
 Created on Thu Nov 29 09:34:16 2018
 
-@author: nahas
+@author: maggie
 """
-#print(5-6)
-#print(8*9)
-#print(6/2)
-#print(5/2)
-#print(5.0/2)
-#print(5%2)
-#print(2*(10+3))
-#print(2**4)
-#
-#
-#age = 5
-#age = "almost three"
-#age = 29.5
-#age = 'I don\'t really know'
-#print(age)
-#print(type(age))
-#
-#
-#print('hello' + ' world')
-#print(('Joke \n') * 3)
-#print('Chen' + str(3))
-#print('hello'.upper() +'\n')
-#print('GOODBYE'.lower())
-#print('\n')
-#print('the lord of the rings'.title())
+
+#simple operations
+print(5-6)
+print(8*9)
+print(6/2)
+print(5/2)
+print(5.0/2)
+print(5%2)
+print(2*(10+3))
+print(2**4)
+
+#practising with variables
+age = 5
+age = "almost three"
+age = 29.5
+age = 'I don\'t really know'
+print(age)
+print(type(age))
+
+#basic string manipulation
+print('hello' + ' world')
+print(('Joke \n') * 3)
+print('Chen' + str(3))
+print('hello'.upper() +'\n')
+print('GOODBYE'.lower())
+print('\n')
+print('the lord of the rings'.title())
 
 
-#s1 = 'hello' + 'world'
-#s2 = 'joke ' * 3
-#s3 = 5
-#print(s1 + s2*10 +'\n')
-##cast it into string:
-#print(s1 + s2 + str(s3))
+s1 = 'hello' + 'world'
+s2 = 'joke ' * 3
+s3 = 5
+print(s1 + s2*10 +'\n')
+#cast it into string:
+print(s1 + s2 + str(s3))
 
-#b1 = '4'
-#b2 = '6'
-#b3 = 8
-##result = b1 + b2 + b3
-##print(result)
-##above would give error, b1 and b2 need to be cast into int()
-#result2 = int(b1) + int(b2) + b3
-#print(result2)
+b1 = '4'
+b2 = '6'
+b3 = 8
+#result = b1 + b2 + b3
+#print(result)
+#above would give error, b1 and b2 need to be cast into int()
+result2 = int(b1) + int(b2) + b3
+print(result2)
 
 
-#strExample = 'a,b,c,d,happy'
-#splitStr = strExample.split(',')
-#print(splitStr)
+strExample = 'a,b,c,d,happy'
+splitStr = strExample.split(',')
+print(splitStr)
 
+#formatting
 #format is a string only method
-#age = 6
-#like = 'painting'
-#
-#age_desc = 'My age is {} and I like {}.'.format(age, like)
-#print(age_desc)
-#age_desc2 = 'My age is {0} and I like {1}.'.format(age, like)
-#print(age_desc2)
+age = 6
+like = 'painting'
 
-##testing comments
-#"""
-#test hahaha
-#commeting some random text
-#right now
-#"""
-#
-##when assigned, it does not count as commented out. which is the reason
-##why when prining test, it is shown in the console
-#test = """sss
-#blabla
-#horror
-#"""
-#print(test)
-#
-#test2 = test.split()
-#print(test2)
+age_desc = 'My age is {} and I like {}.'.format(age, like)
+print(age_desc)
+age_desc2 = 'My age is {0} and I like {1}.'.format(age, like)
+print(age_desc2)
+
+#testing comments
+"""
+test hahaha
+commeting some random text
+right now
+"""
+
+#when assigned, it does not count as commented out. which is the reason
+#why when prining test, it is shown in the console
+test = """sss
+blabla
+horror
+"""
+print(test)
+
+test2 = test.split()
+print(test2)
 
 #homework
 print(10/3)
