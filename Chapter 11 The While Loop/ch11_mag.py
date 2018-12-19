4ccfff79835817cf73e476451d6e782dc909d4ce
Mag Kafel <na.hasiok@gmail.com>
2018-12-19 14:24:11 +0000

    Uncommented ch11

--- Chapter 11 The While Loop/ch11_mag.py
+++ Chapter 11 The While Loop/ch11_mag.py
@@ -4,70 +4,58 @@ Created on Tue Dec 18 09:09:06 2018
 
 @author: mag
 """
+#ex1 repeated division
 x = 33
 while x >= 1:
     print(x, ": ", end=' ')
     x = x / 2  
 print(x)
-#
-##n + (n-1) +...+ 2 + 1
-#
-#def someMaths(n):
-#    numx3 = 0
-#    while n > 0:
-#        numx3 = numx3 + n
-#        n = n - 1
-#    return numx3
-#
-#print(someMaths(1))   
-#print(someMaths(2))
-#print(someMaths(3))
-#print(someMaths(4))
-#print(someMaths(5))
-#print(someMaths(6))
 
-#mark = 1
-#while mark > 0:
-#    mark = int(input("What's your mark: "))
-#    print("Your mark is", mark, end = ". ")
-#    if mark >= 70:
-#        print("First class!")
-#    elif mark >= 40:
-#        print("Pass!")
-#    else:
-#        print("Oh no, that's a fail...")
-   
-#didYouPass = 'Yes'
-#
-#while didYouPass == 'Yes':
-#
-#   mark = int(input('What is your score? '))
-#   
-#   if mark >= 70 and mark <=90:
-#       print('FIRST CLASS')
-#
-#   elif mark >= 40:
-#       print('PASS')
-#
-#   elif mark < 40:
-#       print('FAIL')
-#
-#   didYouPass = input('Did you pass? ')
+#n + (n-1) +...+ 2 + 1
 
-#print("***********************") 
-#i = 55
-#while i > 10:
-#    print(i)
-#    i = i * 0.8
-#    if i == 35.2:
-#        break
-#
-#print("***********************") 
-#while True:
-#    name = input("What's your name? ")
-#    if name == "done":
-#        break
-#    print("Hello", name)
+#ex2 triangular numbers
+def triangular(n):
+    numx3 = 0
+    while n > 0:
+        numx3 = numx3 + n
+        n = n - 1
+    return numx3
+
+print(triangular(1))   
+print(triangular(2))
+print(triangular(3))
+print(triangular(4))
+print(triangular(5))
+print(triangular(6))
+
+#ex3 student marks
+mark = 1
+while mark > 0:
+    mark = int(input("What's your mark: "))
+    print("Your mark is", mark, end = ". ")
+    if mark >= 70:
+        print("First class!")
+    elif mark >= 40:
+        print("Pass!")
+    else:
+        print("Oh no, that's a fail...")  
+
+print("***********************") 
+i = 55
+while i > 10:
+    print(i)
+    i = i * 0.8
+    if i == 35.2:
+        break
+
+
+#ex4 break statement in a while loop
+while True:
+    name = input("What's your name? ")
+    if name == "done":
+        break
+    print("Hello", name)
     
-        
+#ex5 guessing game - in a separate file
+#ex6 dice game - in a separate file       
     
