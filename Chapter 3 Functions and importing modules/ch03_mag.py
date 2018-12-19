f62629186f21124c2ff25f3ad742772425036e8b
Mag Kafel <na.hasiok@gmail.com>
2018-12-19 13:31:09 +0000

    Updated ch03 & 04  by uncommenting

--- Chapter 3 Functions and importing modules/ch03_mag.py
+++ Chapter 3 Functions and importing modules/ch03_mag.py
@@ -2,48 +2,51 @@
 """
 Created on Thu Nov 29 13:54:59 2018
 
-@author: nahas
+@author: maggie
 """
-#print("What is your name?")
-#name = input()
-#
-##name = input("What's your name?")
-##print("Hello {}!".format(name))
-#
-##name3 = input().upper()
-#
-#name2 = name.capitalize()
-#print("Hello {}!".format(name2))
-#
-#city = input("What's your city?")
-#print ("Hello {} from {}.".format(name2,city))
+#input from user
 
-#def hello_world():
-#    print("Hello world!")
-#    addition()
-#
-#def addition():
-#    add2_3 = 2+3
-#    return add2_3
-#
-#print(addition())
-#    
-#hello_world()
+print("What is your name?")
+name = input()
 
-#def hi_maggie():
-#    name = input("What's your name?").capitalize()
-#    print("Hello {}!".format(name))
-##    print("Hello Maggie!")
-#    random1 = input("Provide a random number.")
-#    random2 = input("Provide another random number.")
-#    print(int(random1) + int(random2))
-#    
-#hi_maggie()
+name = input("What's your name? ")
+print("Hello {}!".format(name))
 
-######################################
+
+name2 = name.capitalize()
+print("Hello {}!".format(name2))
+
+city = input("What's your city? ")
+print ("Hello {} from {}.".format(name2,city))
+
+#ex2 first function
+
+def hello_world():
+    print("Hello world!")
+    addition()
+
+def addition():
+    add2_3 = 2+3
+    return add2_3
+
+print(addition())
+hello_world()
+
+def hi_maggie():
+    name = input("What's your name? ").capitalize()
+    print("Hello {}!".format(name))
+#    print("Hello Maggie!")
+    random1 = input("Provide a random number: ")
+    random2 = input("Provide another random number: ")
+    print(int(random1) + int(random2))
+    
+hi_maggie()
+
+#######################################
 def hello_world_3args(a,b,c):
     print ("{}{}{}".format(a,b,c))
-    
+
+#moved to test file: 
 #a1 = "hello "
 #b1 = "world "
 #c1 = "it's "
@@ -56,24 +59,37 @@ def hello_world_3args(a,b,c):
 #hello_world_3args(a3,a2,b2)
 #hello_world_3args(c1,c2,c3)
 
-#############################################
+#ex3   
+print(range(10))
+print(range(1,10))
+print(range(1,10,2))
+
+#ex4 adding two numbers
+def add_two_numbers(a,b):
+#     print (5 + 3)
+     print (a + b)
+     
+a1 = 6
+b1 = 7
+     
+#add_two_numbers()
+add_two_numbers(a1,b1)
+
+#with return
+
+def add_two_numbers2(a,b):
+#     print (5 + 3)
+    return a + b
+     
+a1 = 4
+b1 = 21
+     
+#add_two_numbers()
+print(add_two_numbers2(a1,b1))
+
     
-#print(range(10))
-#print(range(1,10))
-#print(range(1,10,2))
-#
-#def add_two_numbers(a,b):
-##     print (5 + 3)
-#     print (a + b)
-#     
-#a1 = 6
-#b1 = 7
-#     
-##add_two_numbers()
-#add_two_numbers(a1,b1)
-    
-##########################################################
-#
+#temperature conversion exercise
+
 def tempConverter(celsius):
     
     fahrenheit = celsius * 9.0 /5.0 + 32
@@ -86,102 +102,102 @@ userInput = int(input("What's the temperature in your city today?"))
 tempConverter(userInput)
 
 
-#with return#######################
-#userInput = int(input("What's the temperature in your city today?"))
-#
-#def tempConverter2(celsius):
-#    
-#    fahrenheit = celsius * 9.0 /5.0 + 32
-#    kelvin = celsius + 273.15
-#    
-#    return "That's {} degrees in fahrenheit and {} degrees in kelvin.".format(fahrenheit, kelvin)
-#    
-##print(tempConverter2(userInput))
-##result = tempConverter2(userInput)
-##print(result)
+#ex 5 version with return
+userInput = int(input("What's the temperature in your city today?"))
 
-#################################################
-
-#def tempConv(celsius):
-#    fahrenheit = celsius * 9.0 /5.0 + 32
-#    kelvin = celsius + 273.15
-#    return (fahrenheit, kelvin)
-#
-#userInput = 10
-#
-#outputFahr, outputKel = tempConv(userInput)
-#print(outputFahr, outputKel)
-
-##################################################
-#def convTemp():
-#    celsius = int(input("What's the temperature in your city today?"))
-#    fahrenheit = celsius * 9.0 /5.0 + 32
-#    kelvin = celsius + 273.15
-#    print("Converting temp in c to fahrenheit and kelvin...")
-#    print("That's {} degrees in fahrenheit and {} degrees in kelvin.".format(fahrenheit, kelvin))
-#    return (fahrenheit, kelvin)
-#
-##convTemp()
-#kelvin, fahrenheit = convTemp()
-
-########################################
+def tempConverter2(celsius):
     
-#def convert_temp_kelvin(celsius):
-#    kelvin = celsius + 273.15
-#    
-#    return kelvin
-#
-#def convert_temp_fahr(celsius):
-#    fahr = (celsius * 9.0) / 5.0 + 32
-#    
-#    return fahr
-#
-#def convert_kelvin_to_fahrenheit(kelvin):
-#    fahrenheit = (kelvin - 273.15) * 9/5 + 32
-#    
-#    return fahrenheit
-#
-#
-#def convert_temp_kelvin_to_fahr(celsius):
-#    kelvin = convert_temp_kelvin(celsius)
-##    fahr = convert_temp_fahr(celsius)
-#    fahr = convert_kelvin_to_fahrenheit(kelvin)
-#            
-#    print("Converting temp in kelvin to fahrenheit.")
-#    print("Temp in kelvin:", kelvin)
-#    print("Temp in fahrenheit:", fahr)
-#    
-#    mainResult = kelvin
-#    return mainResult
-#
-#userInput = int(input("What's the temperature in your city today?"))
-#mainResult = convert_temp_kelvin_to_fahr(userInput)
-
-########################################
-
-#def convertDistance(miles):
-#    kilometers = int(miles * 1.609)
-#    print("Converting distance in miles to kilometers.")
-#    print("Distance in miles:", miles)
-#    print("Distance in kilometers:", kilometers)
-#    
-#    return ("miles:" + str(miles), "kilometeres" + str(kilometers))
-#
-#convertDistance(10)
-
-########################################
+    fahrenheit = celsius * 9.0 /5.0 + 32
+    kelvin = celsius + 273.15
     
-#def convertDistance():
-#    miles = int(input("How many miles did you run last weekend?"))
-#    kilometers = (miles * 1.61)
-#    print("The distance you run is {} in miles and {} in kilometers.".format(miles, kilometers))
-#    
-#    return (miles, kilometers)
+    return "That's {} degrees in fahrenheit and {} degrees in kelvin.".format(fahrenheit, kelvin)
+    
+print(tempConverter2(userInput))
+result = tempConverter2(userInput)
+print(result)
+
+#another version
+
+def tempConv(celsius):
+    fahrenheit = celsius * 9.0 /5.0 + 32
+    kelvin = celsius + 273.15
+    return (fahrenheit, kelvin)
+
+userInput = 10
+
+outputFahr, outputKel = tempConv(userInput)
+print(outputFahr, outputKel)
+
+#another version
+def convTemp():
+    celsius = int(input("What's the temperature in your city today?"))
+    fahrenheit = celsius * 9.0 /5.0 + 32
+    kelvin = celsius + 273.15
+    print("Converting temp in c to fahrenheit and kelvin...")
+    print("That's {} degrees in fahrenheit and {} degrees in kelvin.".format(fahrenheit, kelvin))
+    return (fahrenheit, kelvin)
+
+#convTemp()
+kelvin, fahrenheit = convTemp()
+
+#another version
+    
+def convert_temp_kelvin(celsius):
+    kelvin = celsius + 273.15
+    
+    return kelvin
+
+def convert_temp_fahr(celsius):
+    fahr = (celsius * 9.0) / 5.0 + 32
+    
+    return fahr
+
+def convert_kelvin_to_fahrenheit(kelvin):
+    fahrenheit = (kelvin - 273.15) * 9/5 + 32
+    
+    return fahrenheit
+
+
+def convert_temp_kelvin_to_fahr(celsius):
+    kelvin = convert_temp_kelvin(celsius)
+#    fahr = convert_temp_fahr(celsius)
+    fahr = convert_kelvin_to_fahrenheit(kelvin)
+            
+    print("Converting temp in kelvin to fahrenheit.")
+    print("Temp in kelvin:", kelvin)
+    print("Temp in fahrenheit:", fahr)
+    
+    mainResult = kelvin
+    return mainResult
+
+userInput = int(input("What's the temperature in your city today?"))
+mainResult = convert_temp_kelvin_to_fahr(userInput)
 #
-#convertDistance()
+#########################################
+#
+def convertDistance(miles):
+    kilometers = int(miles * 1.609)
+    print("Converting distance in miles to kilometers.")
+    print("Distance in miles:", miles)
+    print("Distance in kilometers:", kilometers)
+    
+    return ("miles:" + str(miles), "kilometeres" + str(kilometers))
 
-##########################################
+convertDistance(10)
+#
+#########################################
+    
+def convertDistance():
+    miles = int(input("How many miles did you run last weekend?"))
+    kilometers = (miles * 1.61)
+    print("The distance you run is {} in miles and {} in kilometers.".format(miles, kilometers))
+    
+    return (miles, kilometers)
 
+convertDistance()
+
+#miles - km game
+#
 def convertDistance(miles):
     kilometers = (miles * 1.61)
     return ("The distance you run is {:.1f} in miles and {:.1f} in kilometers.".format(miles, kilometers))
@@ -194,11 +210,11 @@ London_Cardiff = convertDistance(150.6)
 London_Brighton = convertDistance(67)
 London_Birmingham = convertDistance(126)
 
-##importing
-
-#import math
-##print(pi)
-#print(math.pi)
+###importing
+#
+##import math
+###print(pi)
+##print(math.pi)
 
 ##*******different way of importing
 #from math import *
--- Chapter 4 Conditionals/ch04.mag.py
+++ Chapter 4 Conditionals/ch04.mag.py
@@ -4,80 +4,81 @@ Created on Tue Dec  4 11:47:35 2018
 
 @author: nahas
 """
-#import random
-#import datetime
-#import time
-#
-#def luckyNumberRandom():
-#    name = input("Please type your name here: ")
-#    print ("hello " + name.upper())
-#    number = int(input("Please give a number "))
-#    
-#    print("Your lucky number is: " + str(random.randint(1, number)))
-#    
-#startTime = time.time()
-#print("date and time", datetime.datetime.now())
-#print()
-#print("current time", datetime.datetime.now().time())
-#
-#luckyNumberRandom()
-#
-#processTime = time.time()-startTime
-#
-#print()
-#print("Program running time:", round(processTime, 2),"second")
+import random
+import datetime
+import time
 
-################################################################
-
-#age = 15
-#
-#isaTeen = age >= 13 and age <= 19
-#
-#print(isaTeen)
-#
-#age = 22
-#
-#isaTeen = age >= 13 and age <= 19
-#
-#
-#print(isaTeen)
-
-#############################################################
-
-#number = input("Enter a number between 1 and 10: ")
-#number = int(number) 
-#
-#if number > 10:
-#    print("Too high!")
-#elif number <= 0:
-#    print("Too low!")
-#else:
-#    print("Correct.")
+def luckyNumberRandom():
+    name = input("Please type your name here: ")
+    print ("hello " + name.upper())
+    number = int(input("Please give a number "))
     
-##########################################################
+    print("Your lucky number is: " + str(random.randint(1, number)))
+
+#######################################################
+    
+startTime = time.time()
+print("date and time", datetime.datetime.now())
+print()
+print("current time", datetime.datetime.now().time())
+
+luckyNumberRandom()
+
+processTime = time.time()-startTime
+
+print()
+print("Program running time:", round(processTime, 2),"second")
+
+###############################################################
+
+age = 15
+
+isaTeen = age >= 13 and age <= 19
+
+print(isaTeen)
+
+age = 22
+
+isaTeen = age >= 13 and age <= 19
+
+
+print(isaTeen)
+
+#if, elif, else statements
+
+number = input("Enter a number between 1 and 10: ")
+number = int(number) 
+
+if number > 10:
+    print("Too high!")
+elif number <= 0:
+    print("Too low!")
+else:
+    print("Correct.")
+    
+#########################################################
 #if I change the order of elif statement, the result will be different
-#age = input("Enter your age: ")
-#age = int(age) 
-#
-#if age < 13:
-#    print("child")
-#elif age < 18:
-#    print("teen")
-#elif age < 95:
-#    print("adult")
-#else:
-#    print("Most probably you're dead. RIP.")
+age = input("Enter your age: ")
+age = int(age) 
 
-################################################
-#def checkTeen(age):
-#    if age >=13 and age <=19:
-#        print("You're a teen")
-#    else:
-#        print("You're not a teen.")
-#    
-#checkTeen(20)
+if age < 13:
+    print("child")
+elif age < 18:
+    print("teen")
+elif age < 95:
+    print("adult")
+else:
+    print("Most probably you're dead. RIP.")
 
-##################################################
+###############################################
+def checkTeen(age):
+    if age >=13 and age <=19:
+        print("You're a teen")
+    else:
+        print("You're not a teen.")
+    
+checkTeen(20)
+
     
     
     
