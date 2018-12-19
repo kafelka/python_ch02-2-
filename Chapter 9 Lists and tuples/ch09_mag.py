b077e08f6b3677d5ba2b51c1cdffc1962da6008d
Mag Kafel <na.hasiok@gmail.com>
2018-12-19 14:05:00 +0000

    Updated ch09 by uncommenting and adding ex.comments

--- Chapter 9 Lists and tuples/ch09_mag.py
+++ Chapter 9 Lists and tuples/ch09_mag.py
@@ -4,200 +4,201 @@ Created on Wed Dec 12 12:16:04 2018
 
 @author: maggie
 """
-#my_fav_fruit = ["apple", "raspberry", "grapefruit"]
-#print(my_fav_fruit)
-#print(my_fav_fruit[0])
-#
-#x = ["this", 55, "that", my_fav_fruit]
-#print(x)
-#print(x[0])
-#print(x[1])
-#print(x[3])
-#print("****** result for x[-1][-2]:")
-#print(x[-1][-2])
-#print("result of [x[-2][-3]]:")
-#print(x[-2][-3])
-#print(x[3][0])
-#
-#print("result of x[-1][-2][-2][0]")
-#print(x[-1][-2][-2][0])
-##print(x[4])
-#
-#print("*******************")
-#print("print x before removal: ", x)
-#x.remove(my_fav_fruit)
-#print("print x after removal:", x)
-#x[1] = "and"   #replacing/overwriting/updating item with index 1
-#print(x)
-#
-#x.append("again") #appending/extending the list ( as last element)
-#print(x)
-#y = x
-#y = x.append("hello")
-#print(y)
-#print(type(y))
-#print(x)
-#
-#a = [0, 2, 3, 2] #removes the first matching value (not returning)
-#a.remove(2)
-#print(a)
-#
-#b = [3, 2, 2, 1] # removes the item at a specific index (not returning)
-#del b[1]
-#print(b)
-#
-#c = [4, 3, 5]  #removes the item at a specific index and returns it
-#c.pop(1)
-#print(c)
-#
-#d = [1, 2, 3, 4, 5, 6]
-#del d[2:]
-#print(d)
-#
-#print("operations*******************")
-#e = ["the", "cat", "sat"]
-#f = ["on", "the", "mat"]
-#g = e + f
-#print(g)
-#
-#h = 2 * e
-#print(h)
-#print("*******************")
-#print(set(e+f))
-#
-#print("set **********")
-#print("print set(e): ", set(e))   #printing in curly brackets dictionary
-#a = set(e)
-#print("print a:", a)
-#
+#ex1 create a list
+my_fav_fruit = ["apple", "raspberry", "grapefruit"]
+print(my_fav_fruit)
+print(my_fav_fruit[0])
+
+x = ["this", 55, "that", my_fav_fruit]
+print(x)
+print(x[0])
+print(x[1])
+print(x[3])
+print("****** result for x[-1][-2]:")
+print(x[-1][-2])
+print("result of [x[-2][-3]]:")
+print(x[-2][-3])
+print(x[3][0])
+
+print("result of x[-1][-2][-2][0]")
+print(x[-1][-2][-2][0])
+#print(x[4])  list index out of range
+
+#ex2 modify the list
+print("print x before removal: ", x)
+x.remove(my_fav_fruit)
+print("print x after removal:", x)
+x[1] = "and"   #replacing/overwriting/updating item with index 1
+print(x)
+
+x.append("again") #appending/extending the list ( as last element)
+print(x)
+y = x
+y = x.append("hello")
+print(y)
+print(type(y))
+print(x)
+
+a = [0, 2, 3, 2] #removes the first matching value (not returning)
+a.remove(2)
+print(a)
+
+b = [3, 2, 2, 1] # removes the item at a specific index (not returning)
+del b[1]
+print(b)
+
+c = [4, 3, 5]  #removes the item at a specific index and returns it
+c.pop(1)
+print(c)
+
+d = [1, 2, 3, 4, 5, 6]
+del d[2:]
+print(d)
+
+print("operations*******************")
+e = ["the", "cat", "sat"]
+f = ["on", "the", "mat"]
+g = e + f
+print(g)
+
+h = 2 * e
+print(h)
+print(set(e+f))
+
+print("set **********")
+print("print set(e): ", set(e))   #printing in curly brackets dictionary
+a = set(e)
+print("print a:", a)
+
+#ex 3 slicing
 #print("*****slicing******")
-#k = ["this", "and", "that", "once", "again"]
-#print(k)
-#print(k[1:4])
-#print(k[0:0])
-#print(k[0:2])
-#print(k[3:5])
-#print(k[-1:-3])
-#print(k[-3:-1])
-#print(k[-3:])
-#print(k[-5:-2])
-#print(k)
-#
-#z = ["the", "cat", "sat", "on", "the", "mat"]
-#print(z[3:10])
-#print(z[8:10])
+k = ["this", "and", "that", "once", "again"]
+print(k)
+print(k[1:4])
+print(k[0:0])
+print(k[0:2])
+print(k[3:5])
+print(k[-1:-3])
+print(k[-3:-1])
+print(k[-3:])
+print(k[-5:-2])
+print(k)
 
-#
+z = ["the", "cat", "sat", "on", "the", "mat"]
+print(z[3:10])
+print(z[8:10])
+
+#ex4 sorting
 #print("*****sorting******")
-#m = [7,11,3,9,2]
-#print("print m:", m)
-#n = sorted(m)
-#print("print m: ", m)
-#print("print n: ", n)
-#
-#m.sort()
-#print("print m after m.sort: ", m)
-#print("*******************")
-#
-#zz = ["ok", "cs", "yw", "zs", "hd"]
-#print("print original zz :", zz)
-#yy = sorted(zz)
-#print(yy)
-#print(zz)
-#zz.sort()
-#print("print zz after zz.sort(): ", zz)
-#print(zz.sort())
-#
-#print("*******************")
-#r = [7,11,3,9,2]
-#print("print original r: ", r)
-#print(sorted(r))
-#
-#sorted(r,reverse = True) #reversing changes
-#print(r)
-#
-#rr = [7,11,3,9,2]
-#print("print original rr: ", rr)
-#rr.sort()
-#print("print rr after rr.sort: ", rr)
-#
-#rr.sort(reverse = True)
-#print("print reversed rr: ", rr)
-#print("\n")
-#
-#print("REVERSE generic sorted() function")
-#aaa = ["ab", "cs", "yw", "zs", "hd"]
-#aa2 = sorted(aaa, reverse = True)
-#print("after sorted(aaa) st is =:", aaa)
-#print("aa2 = sorted(aaa) is =:", aa2)
-#
-#print("object method .sort() , the same in memory")
-#print("original aaa: ", aaa)
-#aaa.sort(reverse = True)
-#bbb = aaa.sort()
-#print("now aaa is =:", aaa)
-#print("now aa2 is =:", aa2)
-#
-#print("\n")
-#print("generic sorted() function")
-#st = ["ab", "cs", "yw", "zs", "hd"]
-#st2 = sorted(st)
-#print("after sorted(st) st is =:", st)
-#print("st2 = sorted(st) is =:", st2)
-#
-#print("object method .sort() , the same in memory")
-#print("original st: ", st)
-#st.sort()
-#bb = st.sort()
-#print("now st is =:", st)
-#print("now st2 is =:", st2)
+m = [7,11,3,9,2]
+print("print m:", m)
+n = sorted(m)
+print("print m: ", m)
+print("print n: ", n)
 
+m.sort()
+print("print m after m.sort: ", m)
+print("*******************")
 
-#print("****tuples-immutable=read from them but cannot change****")
-#print("example 1")
-#a = [0,1,2,3,4,5,6,7,8,9]
-#print(a)
-#del a[-1]
-#print(a)
-#del a[-3]
-#print(a)
-#del a[-3:]
-#print(a)
-#
-#b = (0,1,2,3,4,5,6,7,8,9)
-##del b[-1]  tuple object does not support item deletion
-#print(b)
-#
-#print("\n")
-#print("example 2")
-#a = [0,1,2,3,4,5,6,7,8,9]
-#a[0] = 50
-#print(a)
-#
-#b = (0,1,2,3,4,5,6,7,8,9)
-##b[0] = 50 tuple object does not support item assignment
-#print(b)
-#
-#print("\n")
-#print("example 3")
-#a.append("z")
-#print(a)
-#
-##b.append("z") #tuple object has no attribute 'append'
-#print(b)
-#
-#c = list(b)
-#print(type(b))
-#print(type(c))
+zz = ["ok", "cs", "yw", "zs", "hd"]
+print("print original zz :", zz)
+yy = sorted(zz)
+print(yy)
+print(zz)
+zz.sort()
+print("print zz after zz.sort(): ", zz)
+print(zz.sort())
 
-#dokonczyc pozniej
+print("*******************")
+r = [7,11,3,9,2]
+print("print original r: ", r)
+print(sorted(r))
+
+sorted(r,reverse = True) #reversing changes
+print(r)
+
+rr = [7,11,3,9,2]
+print("print original rr: ", rr)
+rr.sort()
+print("print rr after rr.sort: ", rr)
+
+rr.sort(reverse = True)
+print("print reversed rr: ", rr)
+print("\n")
+
+print("REVERSE generic sorted() function")
+aaa = ["ab", "cs", "yw", "zs", "hd"]
+aa2 = sorted(aaa, reverse = True)
+print("after sorted(aaa) st is =:", aaa)
+print("aa2 = sorted(aaa) is =:", aa2)
+
+print("object method .sort() , the same in memory")
+print("original aaa: ", aaa)
+aaa.sort(reverse = True)
+bbb = aaa.sort()
+print("now aaa is =:", aaa)
+print("now aa2 is =:", aa2)
+
+print("\n")
+print("generic sorted() function")
+st = ["ab", "cs", "yw", "zs", "hd"]
+st2 = sorted(st)
+print("after sorted(st) st is =:", st)
+print("st2 = sorted(st) is =:", st2)
+
+print("object method .sort() , the same in memory")
+print("original st: ", st)
+st.sort()
+bb = st.sort()
+print("now st is =:", st)
+print("now st2 is =:", st2)
+
+#ex5 create tuples and compare them with list
+print("****tuples-immutable=read from them but cannot change****")
+print("example 1")
+a = [0,1,2,3,4,5,6,7,8,9]
+print(a)
+del a[-1]
+print(a)
+del a[-3]
+print(a)
+del a[-3:]
+print(a)
+
+b = (0,1,2,3,4,5,6,7,8,9)
+#del b[-1]  tuple object does not support item deletion
+print(b)
+
+print("\n")
+print("example 2")
+a = [0,1,2,3,4,5,6,7,8,9]
+a[0] = 50
+print(a)
+
+b = (0,1,2,3,4,5,6,7,8,9)
+#b[0] = 50 tuple object does not support item assignment
+print(b)
+
+print("\n")
+print("example 3")
+a.append("z")
+print(a)
+
+#b.append("z") #tuple object has no attribute 'append'
+print(b)
+
+c = list(b)
+print(type(b))
+print(type(c))
+
+#finish later
 #z = sorted(a, reverse = True)
 #if a == b:
 #    print(True)
 #else:
 #    print(False)
 
-print("\n")
+#ex6 using lambda
 print("*******lambda******")
 
 a = [0,1,2,3,4,5,6,7,8,9]
@@ -214,9 +215,9 @@ print(x2)
 print("*****")
 print(sorted(x2, key=lambda s:s[2][-1][-2]))
 print("*****")
-#print("z is ", z)
-#print("y is ", y)
-#print("x is ", x)
+print("z is ", z)
+print("y is ", y)
+print("x is ", x)
 
 print("*****")
 print(sorted(x2, key=lambda s:s[1]))
@@ -226,21 +227,21 @@ print(sorted(x2, key=lambda s:s[1]))
 print("*****")
 print(sorted(x2, key=lambda s:s[2]))
 print("*****")
-#print(sorted(x2, key=lambda s:s[2][1]))
+print(sorted(x2, key=lambda s:s[2][1]))
 
-#a = [1,2,3,4]
-#b = [3,4,10,3]
-#c = [20,10,30,40]
-#
-#y2 = [("a", 3 ,c), ("c", 1 ,b), ("b", 5, a)]
-#
-#print(sorted(y2, key=lambda s:s[1]))
-#print("*****")
-#print(sorted(y2, key=lambda s:s[2]))
-#print("*****")
-#print(sorted(y2, key=lambda s:s[2][-1]))
+a = [1,2,3,4]
+b = [3,4,10,3]
+c = [20,10,30,40]
 
-print("******new example*******")
+y2 = [("a", 3 ,c), ("c", 1 ,b), ("b", 5, a)]
+
+print(sorted(y2, key=lambda s:s[1]))
+print("*****")
+print(sorted(y2, key=lambda s:s[2]))
+print("*****")
+print(sorted(y2, key=lambda s:s[2][-1]))
+
+print("new example tuples with lambdas")
 
 a = [0,1,2,3,4,5,6,7,8,9]
 b = [0,1,2,3,4,5,6]
@@ -276,23 +277,20 @@ employees = [('Janine Saunders', 50000, 'UX Designer'),
 print (sorted(employees, key = lambda salary:salary[2]))
 
 
+x = ["the", "cat", "on"]
+y = [7,11,3,9,2]
+z = [2,3,7,8,11]
+x2 = [("a",3,z), ("c",1,y), ("b",5,x)]
+sorted(x2)
+print(x2)
 
+z[0]=22
+z[1]=5
 
+x2 = [("a",3,z), ("c",1,y), ("b",5,x)]
 
-#x = ["the", "cat", "on"]
-#y = [7,11,3,9,2]
-#z = [2,3,7,8,11]
-#x2 = [("a",3,z), ("c",1,y), ("b",5,x)]
-#sorted(x2)
+#sorted(x2, key=lambda s:s[2])
 #print(x2)
-#
-#z[0]=22
-#z[1]=5
-#
-#x2 = [("a",3,z), ("c",1,y), ("b",5,x)]
-#
-##sorted(x2, key=lambda s:s[2])
-##print(x2)
-##print("*******************")
-##sorted(x2, key=lambda s:s[2][1])
-##print(x2)
+#print("*******************")
+#sorted(x2, key=lambda s:s[2][1])
+#print(x2)
