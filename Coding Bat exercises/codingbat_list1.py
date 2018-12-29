4cc9250d240f84ceab7522b171ec96294a2097b5
id github: kafelka <na.hasiok@gmail.com>
2018-12-29 01:28:57 +0000

    List1 completed

--- Coding Bat exercises/codingbat_list1.py
+++ Coding Bat exercises/codingbat_list1.py
@@ -14,7 +14,8 @@ Created on Fri Dec 28 23:36:35 2018
 #first_last6([6, 1, 2, 3]) → True
 #first_last6([13, 6, 1, 2, 3]) → False
 
-def first_last6(nums):
+def first_last6(nums): 
+#    return nums[0] == 6 or nums[-1] == 6
     if nums[0] == 6 or nums[-1] == 6:
         return True
     else:
@@ -116,8 +117,9 @@ print("\n")
 #reverse3([7, 0, 0]) → [0, 0, 7]
 
 def reverse3(nums):
-#    return nums.reverse() #prints as NONE cause original list is modified
-#    or overwritten
+#    nums.reverse() #prints as NONE cause original list is overwritten
+#    return nums
+
     return list(reversed(nums))
 #    return nums[::-1] another good solution
 #    nums[::2] gives copy of a list with every second el of the original
@@ -137,8 +139,11 @@ print("\n")
 #max_end3([2, 11, 3]) → [3, 3, 3]
 
 def max_end3(nums):
-    return
-
+    if nums[0] > nums[-1]:
+        return [nums[0]] * 3 
+        # return [nums[0]] * len(nums)   to make more generic
+    else:
+        return [nums[-1]] * 3
 
 print(max_end3([1, 2, 3]))
 print(max_end3([11, 5, 9]))
@@ -147,17 +152,74 @@ print(max_end3([2, 11, 3]))
 print("\n")
 
 
+#Given an array of ints, return the sum of the first 2 elements in 
+#the array. If the array length is less than 2, just sum up the 
+#elements that exist, returning 0 if the array is length 0.
+#sum2([1, 2, 3]) → 3
+#sum2([1, 1]) → 2
+#sum2([1, 1, 1, 1]) → 2
+
+def sum2(nums):
+    if len(nums) < 2:
+        return sum(nums)
+    elif len(nums) == 0:
+        return 0
+    else:
+        return nums[0] + nums[1]
+    
+#    return sum(nums[:2])
+    
+print(sum2([1, 2, 3]))
+print(sum2([1, 1]))
+print(sum2([1, 1, 1, 1]))
+############################
+print("\n")
+
+#Given 2 int arrays, a and b, each length 3, return a new array 
+#length 2 containing their middle elements.
+#middle_way([1, 2, 3], [4, 5, 6]) → [2, 5]
+#middle_way([7, 7, 7], [3, 8, 0]) → [7, 8]
+#middle_way([5, 2, 9], [1, 4, 5]) → [2, 4]
+
+def middle_way(a, b):
+    return [a[1]] + [b[1]]
+
+print(middle_way([1, 2, 3], [4, 5, 6]))
+print(middle_way([7, 7, 7], [3, 8, 0]))
+print(middle_way([5, 2, 9], [1, 4, 5]))
 
 ############################
 print("\n")
 
+#Given an array of ints, return a new array length 2 containing the 
+#first and last elements from the original array. The original array 
+#will be length 1 or more.
+#make_ends([1, 2, 3]) → [1, 3]
+#make_ends([1, 2, 3, 4]) → [1, 4]
+#make_ends([7, 4, 6, 2]) → [7, 2]
+
+def make_ends(nums):
+    return [nums[0]] + [nums[-1]]
+
+print(make_ends([1, 2, 3]))
+print(make_ends([1, 2, 3, 4]))
+print(make_ends([7, 4, 6, 2]))
+
 ############################
 print("\n")
 
+#Given an int array length 2, return True if it contains a 2 or a 3.
+#has23([2, 5]) → True
+#has23([4, 3]) → True
+#has23([4, 5]) → False
 
-############################
-print("\n")
+def has23(nums):
+    if 2 in nums or 3 in nums:
+        return True
+    else:
+        return False
 
+print(has23([2, 5]))
+print(has23([4, 3]))
+print(has23([4, 5]))
 
-############################
-print("\n")
