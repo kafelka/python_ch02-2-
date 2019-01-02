7722190bffc33ab2850dcd9951cba39a4e0c817b
id github: kafelka <na.hasiok@gmail.com>
2019-01-02 22:57:00 +0000

    String2 completed

--- Coding Bat exercises/codingbat_string2.py
+++ Coding Bat exercises/codingbat_string2.py
@@ -125,8 +125,10 @@ print("\n")
 #xyz_there('xyz.abc') → True
 
 def xyz_there(str):
+    if str[:3] == "xyz":
+        return True
     for i in range(len(str)-2):
-        if str[i] != "." and str[i+1:i+3] == "xyx":
+        if str[i] != "." and str[i+1:i+4] == "xyz":
              return True
     return False
 
