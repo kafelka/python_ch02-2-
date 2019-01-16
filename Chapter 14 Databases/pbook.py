703ca9ed4e606da549dd35523c6d43d4ae81696d
Mag Kafel <na.hasiok@gmail.com>
2019-01-16 21:06:51 +0000

    deleted commit details

---
+++ Chapter 14 Databases/pbook.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  9 12:18:10 2019
+
+@author: mag
+"""
+
---
+++ Chapter 16 Flask/myApp/hello.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+app = Flask("MyApp")
+
+#@app.route("/")
+#def hello():
+#    return "<h1>Hello world2!</h1><p>test1<span style='color:blue';>blablabla</span></p><p>test2</p>"
+
+#@app.route("/contactus")
+#def firstPar():
+#    return "<p>phone email address</p>"
+
+
+
+
+#nie dziala ponizsze
+@app.route("/greeting/<greeting>")
+def randomGreeting(greeting):
+    return render_template("hello.html", greeting = greeting)
+
+#@app.route("/")
+#def hello_someone():
+#    return render_template("index.html")
+
+@app.route("/")
+def begin():
+    return render_template("hello.html", greeting = "hello")
+
+@app.route("/confirmation", methods=["POST"])
+def confirmation():
+    form_data = request.form
+    email = form_data["email"]
+    result="All OK"
+    return render_template("confirmation.html", title="Form confirmation", **locals())
+    
+@app.route("/<name>")
+def hello_someone(name):
+    return render_template("hello.html", greeting = "hello", name=name.title())
+
+app.run(debug=True)
+
+
+#playing with css but not a good idea
+#@route("path/to/sheet.css")
+
+
+app.run(debug=True)
