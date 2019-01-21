61aa945da5db67d4436186062803159fa56d0f2a
Mag Kafel <na.hasiok@gmail.com>
2019-01-21 16:17:30 +0000

    updated pollution file

--- Misc/api_exercises/pollution.py
+++ Misc/api_exercises/pollution.py
@@ -1,7 +1,12 @@
 import requests
 import json
-import datetime
+
 from time import strftime
+import sqlite3
+import time
+import datetime
+import random
+from datetime import datetime
 
 #Get Carbon Intensity data for past 24h for specified postcode
 #response = requests.get("https://api.carbonintensity.org.uk/regional/intensity/2019-01-21T09:00Z/pt12h/postcode/NW6")
@@ -15,19 +20,6 @@ print(response3.status_code)
 print(response3.headers["content-type"])
 print(response3.text)
 print("************")
-#city = data["data"][0]["shortname"]
-#print("City:", city)
-#postcode = data["data"][0]["postcode"]
-#print("Postcode:", postcode)
-#date = data["data"][0]["data"][0]["from"]
-#print("date:", date)
-#
-#intensity = data["data"][0]["data"][0]["intensity"]
-#print("Intensity:", intensity)
-#forecast = data["data"][0]["data"][0]["intensity"]["forecast"]
-#print("Forecast:", forecast)
-#index = data["data"][0]["data"][0]["intensity"]["index"]
-#print("Index:", index)
 
 city = data["data"]["shortname"]
 print(city)
@@ -42,7 +34,6 @@ print("************")
 test2keys = list(test2.keys())
 test2values  = list(test2.values())
 
-
 for i in test2keys:
     print("keys:", i)
     
@@ -53,6 +44,38 @@ for i in test2values:
 dateTest = test2["from"]
 print(dateTest)
 
+data_czas = datetime.strptime(dateTest, '%Y-%m-%dT%H:%MZ')
+print(data_czas)
+sama_data = datetime.strptime(dateTest, '%Y-%m-%dT%H:%MZ').date()
+print(sama_data)
 
 
+for i in data["data"]["data"]:
+   print("date:", i["from"], "\n", "forecast:", i["intensity"]["forecast"], "\n", "index:", i["intensity"]["index"], "\n",
+         i["generationmix"][0]["fuel"], i["generationmix"][0]["perc"], "\n",
+         i["generationmix"][1]["fuel"], i["generationmix"][1]["perc"], "\n",
+         i["generationmix"][2]["fuel"], i["generationmix"][2]["perc"], "\n",
+         i["generationmix"][3]["fuel"], i["generationmix"][3]["perc"], "\n",
+         i["generationmix"][4]["fuel"], i["generationmix"][4]["perc"], "\n",
+         i["generationmix"][5]["fuel"], i["generationmix"][5]["perc"], "\n",
+         i["generationmix"][6]["fuel"], i["generationmix"][6]["perc"], "\n",
+         i["generationmix"][7]["fuel"], i["generationmix"][7]["perc"], "\n",
+         i["generationmix"][8]["fuel"], i["generationmix"][8]["perc"]
+         )
+   
+   
+conn = sqlite3.connect("natGrid.db")
+c = conn.cursor()
 
+#def create_table():
+#    c.execute("CREATE TABLE IF NOT EXISTS pollution(datestamp TEXT, forecast REAL, index TEXT, biomass REAL, coal REAL, imports REAL, gas REAL, nuclear REAL, other REAL, hydro REAL, solar REAL, wind REAL)")
+#    conn.commit()
+#     
+##create_table()
+#
+#def carbIntensity_data_entry():
+#    c.execute('''INSERT INTO pollution(datestamp, forecast, index, biomass, coal, imports, gas, nuclear, other, hydro, solar, wind) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+#                  
+#
+##    conn.commit()
+   
