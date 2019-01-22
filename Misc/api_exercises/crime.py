16575f7d2af9fa27b5c52d3bed85ec0c5466b939
Mag Kafel <na.hasiok@gmail.com>
2019-01-22 21:54:04 +0000

    Updated py file, db populated with data

--- Misc/api_exercises/crime.py
+++ Misc/api_exercises/crime.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from collections import Counter  #counts number of occuranes
+from collections import Counter  #counts number of occurances
 from datetime import timedelta, datetime
 from dateutil.relativedelta import relativedelta
 import sqlite3
@@ -8,8 +8,8 @@ import sqlite3
 #Crimes at street-level; either within a 1 mile radius of a single point, or within a custom area.
 #The street-level crimes returned in the API are only an approximation of where the actual crimes occurred, they are not the exact locations. 
 
-response = requests.get("https://data.police.uk/api/crimes-street/all-crime?lat=51.5517&lng=-0.1588&date=2018-11")
-data = response.json()
+#response = requests.get("https://data.police.uk/api/crimes-street/all-crime?lat=51.5517&lng=-0.1588&date=2018-11")
+#data = response.json()
 #print(data)
 
 #endpoint = "https://data.police.uk/api/crimes-street/all-crime"
@@ -22,20 +22,20 @@ data = response.json()
 #print(response.headers["content-type"])
 #print(response.text)
 
-crimeRecord = data[0]
-print(crimeRecord)
-crimeCategory = crimeRecord["category"]
-print(crimeCategory)
+#crimeRecord = data[0]
+#print(crimeRecord)
+#crimeCategory = crimeRecord["category"]
+#print(crimeCategory)
 
-def showCrimeSummary(data):
-    crimeCategories = Counter([c["category"] for c in data])
+def showCrimeSummary(data): 
+    crimeCategories = Counter([c["category"] for c in data]) ##counts number of occurances -> categories of crime
     return crimeCategories
 
 #print("Camden:")    
 #showCrimeSummary(data)
 
-response = requests.get("https://data.police.uk/api/crimes-street/all-crime?lat=51.53787&lng=-0.1929&date=2018-11")
-data2 = response.json()
+#response = requests.get("https://data.police.uk/api/crimes-street/all-crime?lat=51.53787&lng=-0.1929&date=2018-11")
+#data2 = response.json()
 
 #print("NW64HJ")  
 #showCrimeSummary(data2)
@@ -43,30 +43,30 @@ data2 = response.json()
 startDate = datetime.strptime("2018-01-01", '%Y-%m-%d').date() #date as date, not string
 endDate = datetime.strptime("2018-11-30", '%Y-%m-%d').date()
 
-counter = 0 #counter for days between start and end dates
-crimeCatSummary = Counter([])
+counter = 0 #counter for months between start and end dates
+crimeCatSummary = Counter() 
 result = {}
-while startDate + relativedelta(months = counter) <= endDate: #timedelta = duration in days, counter = number of days
+while startDate + relativedelta(months = counter) <= endDate: #relativedelta = duration in months, counter = number of months
     currentDate = (startDate + relativedelta(months = counter)).strftime("%Y-%m") #converted into string
-    counter += 1 #advance to the next day
-    print("For month:", currentDate)
+    counter += 1 #advance to the next month
+    print("\nFor month:", currentDate)
     response = requests.get(f"https://data.police.uk/api/crimes-street/all-crime?lat=51.53787&lng=-0.1929&date={currentDate}")
     data = response.json()
-    monthData = showCrimeSummary(data)
-    result[currentDate] = monthData
+    monthData = showCrimeSummary(data) #see above
+    result[currentDate] = monthData #key = result[currentDate], value = monthData
     print("Monthly data:", monthData)
-    crimeCatSummary.update(monthData)
+    crimeCatSummary.update(monthData) #sum of all monthly data
 
-print("Crime categories:", crimeCatSummary.keys())
+print("Crime categories:", crimeCatSummary.keys()) #keys for all data
 crimeColumns = [key + " REAL" for key in crimeCatSummary.keys()]
-columns = ", ".join(crimeColumns).replace("-", "_")
+columns = ", ".join(crimeColumns).replace("-", "_")  #crimeColumns into 1 string and then "-" replaced with "_"
 
 conn = sqlite3.connect("natGrid.db")
 c = conn.cursor()
 
 def create_table():
     print("Creating table...")
-    c.execute("DROP TABLE IF EXISTS crime")
+    c.execute("DROP TABLE IF EXISTS crime") #remove table
     c.execute(f"CREATE TABLE IF NOT EXISTS crime(month TEXT, {columns})")
     
     conn.commit()
@@ -75,12 +75,14 @@ create_table()
 
 def crime_data_entry():
     print("Populating DB with data...")
-    for item in results:
-        c.execute('''INSERT INTO pollution(datestamp, forecast, intensityIndex, biomass, coal, imports, gas, nuclear, other, hydro, solar, wind) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                 (item["time"].strftime("%Y-%m-%d %H:00"), item['intensityForecast'], item['intensityIndex'],
-                 item['fuelBiomass'], item['fuelCoal'], item['fuelImports'], item['fuelGas'],
-                 item['fuelNuclear'], item['fuelOther'], item['fuelHydro'], item['fuelSolar'],
-                 item['fuelWind']))
+    for month, data in result.items(): # m,d = kv pair, data = monthData in while loop
+        questionMarks = "?, " * len(data.keys()) + "?"
+        columnsString = ", ".join(data.keys()).replace("-", "_")
+        print("\nData:", data)
+        print(f"\nQuery: INSERT INTO crime(month, {columnsString}) VALUES({questionMarks})", (month, *data.values())) 
+        c.execute(f'''INSERT INTO crime(month, {columnsString}) VALUES({questionMarks})''',
+                 (month, *data.values()))   #star * unpacks the sequence/collection
+        
     conn.commit()
         
 crime_data_entry()
